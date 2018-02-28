775a0b18570fab6da2c8cc7607dd34cc8d895c63
Maddy Dannenberg <mmdberg@gmail.com>
2018-02-28 11:20:56 -0700

    Restart html framework

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 
 
-pageLoad();
-$('.header__nav-bar__showHamburger').click(showMenu);
-$('.header__nav-bar__exitHamburger').click(hideMenu);
+// pageLoad();
+// $('.header__nav-bar__showHamburger').click(showMenu);
+// $('.header__nav-bar__exitHamburger').click(hideMenu);
 
-function pageLoad() {
- $('.header__nav-bar__exitHamburger').hide();
- $('.header__hamburger').hide();
-}
+// function pageLoad() {
+//  $('.header__nav-bar__exitHamburger').hide();
+//  $('.header__hamburger').hide();
+// }
 
-function showMenu() {
-  $('h1').hide();
-  $('h2').hide();
-  $('.header__hamburger').slideToggle('slow', function() {
-  $('.header__nav-bar__exitHamburger').show();
-  $('.header__nav-bar__showHamburger').hide();
-  })
-}
+// function showMenu() {
+//   $('h1').hide();
+//   $('h2').hide();
+//   $('.header__hamburger').slideToggle('slow', function() {
+//   $('.header__nav-bar__exitHamburger').show();
+//   $('.header__nav-bar__showHamburger').hide();
+//   })
+// }
 
-function hideMenu() {
-  $('.header__hamburger').slideToggle('slow', function() {
-  $('h1').show();
-  $('h2').show();
-  $('.header__nav-bar__exitHamburger').hide();
-  $('.header__nav-bar__showHamburger').show();
-  })
-}
+// function hideMenu() {
+//   $('.header__hamburger').slideToggle('slow', function() {
+//   $('h1').show();
+//   $('h2').show();
+//   $('.header__nav-bar__exitHamburger').hide();
+//   $('.header__nav-bar__showHamburger').show();
+//   })
+// }
